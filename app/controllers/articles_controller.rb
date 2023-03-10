1559a0c20817efd38638577a1330e1810ef13190
Talha Tariq <talhahmttk@gmail.com>
2023-03-10 05:05:48 -0500

    display validation messages and created edit and update methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,31 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article=Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
         @article=Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        redirect_to @article
+        if @article.save
+           flash[:notice] = "Article was saved successfully."
+          redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article updated successfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 
 
