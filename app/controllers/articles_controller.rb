c7a8e287e591fa52e7a22f03d55b02a10d9a48da
Talha Tariq <talhahmttk@gmail.com>
2023-03-09 21:44:24 -0500

    added new and create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,15 @@ class ArticlesController < ApplicationController
         @article=Article.all
     end
 
+    def new
+
+    end
+
+    def create
+        @article=Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
+    end
+
+
 end
