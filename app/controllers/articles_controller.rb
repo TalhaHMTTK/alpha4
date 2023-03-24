f86767e72aa986f9da337f2c687d1fc4dc7dc9f4
Talha Tariq <talhahmttk@gmail.com>
2023-03-24 07:40:52 -0400

    added some modifications for security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :destroy, :update]
     
     def show
         
@@ -20,7 +22,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article=Article.new(article_params)
-        @article.user =User.find(9)
+        @article.user = current_user
         if @article.save
            flash[:notice] = "Article was saved successfully."
           redirect_to @article
@@ -53,6 +55,13 @@ end
 
 def article_params
     params.require(:article).permit(:title, :description)
+end
+
+def require_same_user
+if current_user != @article.user
+    flash[:alert] = "You can only edit or delete your own article"
+    redirect_to @article
+end
 end
 
 end
