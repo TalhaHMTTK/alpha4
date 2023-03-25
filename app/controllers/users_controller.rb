95377ddb934af6c0e366847dfb4bfdb83156aa98
Talha Tariq <talhahmttk@gmail.com>
2023-03-24 22:50:55 -0400

    added admin functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-   before_action :set_user, only: [:show, :edit, :update]
+   before_action :set_user, only: [:show, :edit, :update, :destroy]
+   before_action :require_user, only: [:edit, :update]
+   before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -38,6 +40,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+         @user.destroy
+         session[:user_id] = nil if @user == current_user
+         flash[:notice] = "Account and all associated articles deleted successfully"
+         redirect_to articles_path
+    end
+
     private
 
     def user_params
@@ -46,5 +55,12 @@ class UsersController < ApplicationController
 
     def set_user
         @user= User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete only your account"
+            redirect_to @user
+        end
     end
 end
