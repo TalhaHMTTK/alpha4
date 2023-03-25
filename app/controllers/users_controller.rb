9831a8fdd7b05dadd6360d2d257af06d79c4011a
Talha Tariq <talhahmttk@gmail.com>
2023-03-25 01:09:11 -0400

    colors of flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
     def destroy
          @user.destroy
          session[:user_id] = nil if @user == current_user
-         flash[:notice] = "Account and all associated articles deleted successfully"
+         flash[:alert] = "Account and all associated articles deleted successfully"
          redirect_to articles_path
     end
 
